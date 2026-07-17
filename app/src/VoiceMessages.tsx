bbdeef4c822f4351203e1b26128d51533055fca7
tihomirtodorov6-stack <tihomir.todorov6@icloud.com>
2026-07-17 16:26:11 +0100

    Create VoiceMessages.tsx

---
+++ app/src/VoiceMessages.tsx
@@ -0,0 +1,31 @@
+export default function VoiceMessages() {
+  return (
+    <div>
+      <h1>Voice Messages</h1>
+
+      <button>
+        🎙️ Record
+      </button>
+
+      <div>
+        <p>
+          Voice message 00:15
+        </p>
+
+        <button>
+          Play
+        </button>
+      </div>
+
+      <div>
+        <p>
+          Voice message 00:08
+        </p>
+
+        <button>
+          Play
+        </button>
+      </div>
+    </div>
+  );
+}
