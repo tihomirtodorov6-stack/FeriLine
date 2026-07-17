d5e5f7618cea727ba15f3a0b799d19627a7e6747
tihomirtodorov6-stack <tihomir.todorov6@icloud.com>
2026-07-17 16:31:36 +0100

    Create Help.tsx

---
+++ app/src/Help.tsx
@@ -0,0 +1,28 @@
+export default function Help() {
+  return (
+    <div>
+      <h1>Help & Support</h1>
+
+      <div>
+        <p>❓ Frequently Asked Questions</p>
+        <button>
+          View FAQ
+        </button>
+      </div>
+
+      <div>
+        <p>✉️ Contact Support</p>
+        <button>
+          Send message
+        </button>
+      </div>
+
+      <div>
+        <p>🐞 Report a problem</p>
+        <button>
+          Report
+        </button>
+      </div>
+    </div>
+  );
+}
