7798152356fe37e70d7a3ab60d67ec7cfb4d5130
tihomirtodorov6-stack <tihomir.todorov6@icloud.com>
2026-07-17 16:22:58 +0100

    Create Contacts.tsx

---
+++ app/src/Contacts.tsx
@@ -0,0 +1,32 @@
+export default function Contacts() {
+  return (
+    <div>
+      <h1>Contacts</h1>
+
+      <div>
+        <p>👤 Alex</p>
+        <button>
+          Message
+        </button>
+      </div>
+
+      <div>
+        <p>👤 Maria</p>
+        <button>
+          Message
+        </button>
+      </div>
+
+      <div>
+        <p>👤 John</p>
+        <button>
+          Message
+        </button>
+      </div>
+
+      <button>
+        Add contact
+      </button>
+    </div>
+  );
+}
