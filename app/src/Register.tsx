f57706f94a5ea83353df646e3989fd3bc04ca321
tihomirtodorov6-stack <tihomir.todorov6@icloud.com>
2026-07-17 14:13:00 +0100

    Create Register.tsx

---
+++ app/src/Register.tsx
@@ -0,0 +1,20 @@
+export default function Register() {
+  return (
+    <div>
+      <h1>Register</h1>
+
+      <p>
+        Enter your phone number to create your FeriLine account.
+      </p>
+
+      <input
+        type="tel"
+        placeholder="Phone number"
+      />
+
+      <button>
+        Send code
+      </button>
+    </div>
+  );
+}
