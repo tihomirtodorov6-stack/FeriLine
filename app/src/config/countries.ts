7aa79b12bc155d742b0e0763224dff3e922ca529
tihomirtodorov6-stack <tihomir.todorov6@icloud.com>
2026-07-17 15:07:58 +0100

    Create countries.ts

---
+++ app/src/config/countries.ts
@@ -0,0 +1,37 @@
+export const countries = [
+  {
+    name: "Bulgaria",
+    code: "+359",
+    flag: "🇧🇬"
+  },
+  {
+    name: "United Kingdom",
+    code: "+44",
+    flag: "🇬🇧"
+  },
+  {
+    name: "United States",
+    code: "+1",
+    flag: "🇺🇸"
+  },
+  {
+    name: "Germany",
+    code: "+49",
+    flag: "🇩🇪"
+  },
+  {
+    name: "France",
+    code: "+33",
+    flag: "🇫🇷"
+  },
+  {
+    name: "Spain",
+    code: "+34",
+    flag: "🇪🇸"
+  },
+  {
+    name: "Italy",
+    code: "+39",
+    flag: "🇮🇹"
+  }
+];
