4ad7b7b0415044180a1647a5808a008a8516322c
tihomirtodorov6-stack <tihomir.todorov6@icloud.com>
2026-07-17 16:30:54 +0100

    Create Theme.tsx

---
+++ app/src/Theme.tsx
@@ -0,0 +1,25 @@
+export default function Theme() {
+  return (
+    <div>
+      <h1>Theme</h1>
+
+      <div>
+        <button>
+          ☀️ Light mode
+        </button>
+      </div>
+
+      <div>
+        <button>
+          🌙 Dark mode
+        </button>
+      </div>
+
+      <div>
+        <button>
+          Follow system
+        </button>
+      </div>
+    </div>
+  );
+}
