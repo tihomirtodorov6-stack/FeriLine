45e9506fd71ef540ffb20a7ab038b54674cae90e
tihomirtodorov6-stack <tihomir.todorov6@icloud.com>
2026-07-17 15:19:19 +0100

    Update CountrySelector.tsx

--- app/src/components/CountrySelector.tsx
+++ app/src/components/CountrySelector.tsx
@@ -1,1 +1,13 @@
+import { countries } from "../config/countries";
 
+export default function CountrySelector() {
+  return (
+    <select>
+      {countries.map((country) => (
+        <option key={country.code}>
+          {country.flag} {country.name} {country.code}
+        </option>
+      ))}
+    </select>
+  );
+}
