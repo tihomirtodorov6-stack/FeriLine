5835d74c83767d833521413079e4d15c25ffcefa
tihomirtodorov6-stack <tihomir.todorov6@icloud.com>
2026-07-17 14:21:47 +0100

    Update App.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,16 @@
+import Register from "./Register";
+
 export default function App() {
   return (
     <div>
       <h1>FeriLine</h1>
       <p>Connect. Chat. Share.</p>
-      <button>Start</button>
+
+      <button>
+        Start
+      </button>
+
+      <Register />
     </div>
   );
 }
