c251f6e96d20cd512463dddf06e7307eb0790272
tihomirtodorov6-stack <tihomir.todorov6@icloud.com>
2026-07-17 16:24:30 +0100

    Create Groups.tsx

---
+++ app/src/Groups.tsx
@@ -0,0 +1,27 @@
+export default function Groups() {
+  return (
+    <div>
+      <h1>Groups</h1>
+
+      <div>
+        <p>👥 Family</p>
+        <p>5 members</p>
+        <button>
+          Open group
+        </button>
+      </div>
+
+      <div>
+        <p>👥 Friends</p>
+        <p>12 members</p>
+        <button>
+          Open group
+        </button>
+      </div>
+
+      <button>
+        Create group
+      </button>
+    </div>
+  );
+}
