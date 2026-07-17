faf33edecf64a7afd762fdd38f4748a5599a084f
tihomirtodorov6-stack <tihomir.todorov6@icloud.com>
2026-07-17 15:58:26 +0100

    Create ProfileSetup.tsx

---
+++ app/src/ProfileSetup.tsx
@@ -0,0 +1,25 @@
+export default function ProfileSetup() {
+  return (
+    <div>
+      <h1>Create profile</h1>
+
+      <p>
+        Set up your FeriLine profile.
+      </p>
+
+      <input
+        type="text"
+        placeholder="Your name"
+      />
+
+      <input
+        type="text"
+        placeholder="Username"
+      />
+
+      <button>
+        Continue
+      </button>
+    </div>
+  );
+}
