e9bc9fb2685c721c38366e0ee95bc9855941ed1e
tihomirtodorov6-stack <tihomir.todorov6@icloud.com>
2026-07-17 16:32:19 +0100

    Create Backup.tsx

---
+++ app/src/Backup.tsx
@@ -0,0 +1,27 @@
+export default function Backup() {
+  return (
+    <div>
+      <h1>Backup</h1>
+
+      <div>
+        <p>☁️ Backup chats</p>
+        <button>
+          Create backup
+        </button>
+      </div>
+
+      <div>
+        <p>♻️ Restore data</p>
+        <button>
+          Restore
+        </button>
+      </div>
+
+      <div>
+        <p>
+          Last backup: Not available
+        </p>
+      </div>
+    </div>
+  );
+}
