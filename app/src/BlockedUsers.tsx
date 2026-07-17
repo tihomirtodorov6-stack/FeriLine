edc543cdf1e8de281cdbe93a55f26131c861a5da
tihomirtodorov6-stack <tihomir.todorov6@icloud.com>
2026-07-17 16:30:14 +0100

    Create BlockedUsers.tsx

---
+++ app/src/BlockedUsers.tsx
@@ -0,0 +1,15 @@
+export default function BlockedUsers() {
+  return (
+    <div>
+      <h1>Blocked Users</h1>
+
+      <div>
+        <p>👤 No blocked users</p>
+      </div>
+
+      <button>
+        Block user
+      </button>
+    </div>
+  );
+}
