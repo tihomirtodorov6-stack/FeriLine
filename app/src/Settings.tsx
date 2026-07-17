1907a5d2b7e9cd40357339376bf0bd46c3e1486d
tihomirtodorov6-stack <tihomir.todorov6@icloud.com>
2026-07-17 16:22:02 +0100

    Create Settings.tsx

---
+++ app/src/Settings.tsx
@@ -0,0 +1,37 @@
+export default function Settings() {
+  return (
+    <div>
+      <h1>Settings</h1>
+
+      <div>
+        <button>
+          Account
+        </button>
+      </div>
+
+      <div>
+        <button>
+          Privacy
+        </button>
+      </div>
+
+      <div>
+        <button>
+          Notifications
+        </button>
+      </div>
+
+      <div>
+        <button>
+          Security
+        </button>
+      </div>
+
+      <div>
+        <button>
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+}
