8e151cd254fccc85cf9bfb3f02bca9ffd2926d70
tihomirtodorov6-stack <tihomir.todorov6@icloud.com>
2026-07-17 16:45:58 +0100

    Create AppMenu.tsx

---
+++ app/src/AppMenu.tsx
@@ -0,0 +1,31 @@
+export default function AppMenu() {
+  return (
+    <div>
+      <h1>FeriLine Menu</h1>
+
+      <button>
+        Chats
+      </button>
+
+      <button>
+        Contacts
+      </button>
+
+      <button>
+        Calls
+      </button>
+
+      <button>
+        Groups
+      </button>
+
+      <button>
+        Profile
+      </button>
+
+      <button>
+        Settings
+      </button>
+    </div>
+  );
+}
