68ecbc2f6338381a0abf736febf0791fef45e484
tihomirtodorov6-stack <tihomir.todorov6@icloud.com>
2026-07-17 16:05:38 +0100

    Create ChatRoom.tsx

---
+++ app/src/ChatRoom.tsx
@@ -0,0 +1,21 @@
+export default function ChatRoom() {
+  return (
+    <div>
+      <h1>Chat with Alex</h1>
+
+      <div>
+        <p>Alex: Hello!</p>
+        <p>You: Hi, welcome to FeriLine</p>
+      </div>
+
+      <input
+        type="text"
+        placeholder="Type a message..."
+      />
+
+      <button>
+        Send
+      </button>
+    </div>
+  );
+}
