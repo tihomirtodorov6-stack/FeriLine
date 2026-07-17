437a4a29531f55919a45d45a319554b4cf1cc023
tihomirtodorov6-stack <tihomir.todorov6@icloud.com>
2026-07-17 16:28:41 +0100

    Create About.tsx

---
+++ app/src/About.tsx
@@ -0,0 +1,27 @@
+export default function About() {
+  return (
+    <div>
+      <h1>FeriLine</h1>
+
+      <p>
+        Private messaging app for everyone.
+      </p>
+
+      <p>
+        Version 1.0
+      </p>
+
+      <p>
+        Secure chats, calls, groups and file sharing.
+      </p>
+
+      <button>
+        Terms of Service
+      </button>
+
+      <button>
+        Privacy Policy
+      </button>
+    </div>
+  );
+}
