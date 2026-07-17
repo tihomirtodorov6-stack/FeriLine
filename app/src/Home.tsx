f275df4e1edb211c7e961074988abf76245bbeba
tihomirtodorov6-stack <tihomir.todorov6@icloud.com>
2026-07-17 16:46:41 +0100

    Create Home.tsx

---
+++ app/src/Home.tsx
@@ -0,0 +1,23 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to FeriLine</h1>
+
+      <p>
+        Connect with people everywhere.
+      </p>
+
+      <button>
+        Start chatting
+      </button>
+
+      <button>
+        Open contacts
+      </button>
+
+      <button>
+        Make a call
+      </button>
+    </div>
+  );
+}
