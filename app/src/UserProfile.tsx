11623aa70f70a35a08b7c335b02452280b87f6a4
tihomirtodorov6-stack <tihomir.todorov6@icloud.com>
2026-07-17 16:21:07 +0100

    Create UserProfile.tsx

---
+++ app/src/UserProfile.tsx
@@ -0,0 +1,29 @@
+export default function UserProfile() {
+  return (
+    <div>
+      <h1>My Profile</h1>
+
+      <div>
+        <p>📷 Profile photo</p>
+      </div>
+
+      <input
+        type="text"
+        placeholder="Name"
+      />
+
+      <input
+        type="text"
+        placeholder="Status"
+      />
+
+      <p>
+        Phone: +000000000
+      </p>
+
+      <button>
+        Edit profile
+      </button>
+    </div>
+  );
+}
