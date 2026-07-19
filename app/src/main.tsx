2d76447b39d1e2a3d3c9778b7ea7a6e4417e66b9
tihomirtodorov6-stack <tihomir.todorov6@icloud.com>
2026-07-19 16:56:45 +0100

    Update main.tsx

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js");
+  });
+}
+
 ReactDOM.createRoot(
   document.getElementById("root")!
 ).render(
