008f0121978d9994c9f774747428fcf597d23c34
tihomirtodorov6-stack <tihomir.todorov6@icloud.com>
2026-07-20 14:14:28 +0100

    Create Call.tsx

---
+++ app/src/Call.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Call() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column",
+        background: "#111",
+        color: "#fff",
+      }}
+    >
+      <h2>FeriLine Call</h2>
+      <p>Подготовка за гласово обаждане...</p>
+    </div>
+  );
+}
