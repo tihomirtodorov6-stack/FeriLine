10a80e3aa0f02b5d21856b11080df6ae0bd4005a
tihomirtodorov6-stack <tihomir.todorov6@icloud.com>
2026-07-24 20:09:34 +0100

    Create Chatroom.tsx

---
+++ app/src/Chatroom.tsx
@@ -0,0 +1,8 @@
+export default function ChatRoom() {
+  return (
+    <div style={{ padding: 20, color: 'white', background: '#09090b', height: '100vh' }}>
+      <h1>ChatRoom работи</h1>
+      <p>Билдът е оправен. Сега ще върнем логиката.</p>
+    </div>
+  );
+}
